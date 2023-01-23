6b9efeb8862b630d06e73f928b0222bda6b25c8a
Danillo Henrique Zezepanski <danillohzd.real@gmail.com>
2023-01-23 02:07:32 -0300

    atualização profile 0.0.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,96 @@
 
-var divProfileText = document.querySelector("#textprof");
-var text = "JavaScript é muito bom!"
-var interval = 200;
+var divBemvindoText = document.querySelector("#textBemvindo");
+var divEuText = document.querySelector("#textEu");
+var divDevText = document.querySelector("#textDev");
+var text1 = "Seja Bem vindo!"
+var text2 = "Eu me chamo Danillo Henrique"
+var text3 = "Desenvolvedor Front-end"
+var interval = 50;
+var interval1 = 150;
+var interval2 = 150;
 
-function showtext(divProfileText, text, interval) {
-  var char = text.split("").reverse();
-  
-  var typer = setInterval(function() {
-  
-    if (!char.length) {
-        return clearInterval(typer)
-    }
-    
-    var next = char.pop();
-    
-    divProfileText.innerHTML += next;
-    
-  }, interval);
-  
+function showtext() {
+    var char1 = text1.split("").reverse();
+
+    var typer1 = setInterval(function () {
+
+        if (!char1.length) {
+            return clearInterval(typer1)
+        }
+
+        var next1 = char1.pop();
+
+        divBemvindoText.innerHTML += next1;
+
+    }, interval);
+
+    var char2 = text2.split("").reverse();
+
+    var typer2 = setInterval(function () {
+
+        if (!char2.length) {
+            return clearInterval(typer2)
+        }
+
+        var next2 = char2.pop();
+
+        divEuText.innerHTML += next2;
+
+    }, interval1);
+
+    var char3 = text3.split("").reverse();
+
+    var typer3 = setInterval(function () {
+
+        if (!char3.length) {
+            return clearInterval(typer3)
+        }
+
+        var next3 = char3.pop();
+
+        divDevText.innerHTML += next3;
+
+    }, interval2);
+
 }
+
+showtext(divBemvindoText, text1, interval)
+
 
 function clickMenu() {
 
-    var opacity = opcoes.style.opacity
 
     if (opcoes.style.visibility == 'visible') {
-        opcoes.style.visibility = 'hidden'
-        opcoes.style.left = '-30%'
-        opcoes.style.transition = 'visibility 1s, '
-        menuIcon.style.backgroundColor = '#011420'
+        opcoes.style.cssText = 'visibility: hidden;' + 'left: -30%;' + 'transition: visibility 1s, left 1s;';
+
+        menuIcon.style.backgroundColor = '#011420';
 
 
 
-        } else {
-        opcoes.style.visibility = 'visible'
-        opcoes.style.left = '-0%'
-        opcoes.style.transition = 'visibility 1s, left 1s'
+    } else {
+        opcoes.style.cssText = 'visibility: visible;' + 'left: 0%;' + 'transition: visibility 1s, left 1s;';
+
         menuIcon.style.backgroundColor = '#495053';
-        
+
 
 
 
     }
-    
+
 }
 
 function clickProfile() {
 
-    var textComplete = false
+    let profile = document.getElementById('profile')
 
-    if(textComplete == false){
-        showtext(divProfileText, text, interval);
-        
+    if (profile.style.visibility == 'hidden') {
+        profile.style = 'visibility: visible;' + 'opacity: 1 ;' + 'transition: visibility 0s, opacity 0.8s linear;'
+
     }
-    
-    
-    if (profile.style.visibility == 'visible') {
-        profile.style.visibility = 'hidden'
-        profile.style.opacity = 0
-        profile.style.transition = 'visibility 1s, opacity 0.8s linear'
-        
-        
+    else {
 
-    } else {
+        profile.style = 'visibility: hidden;' + 'opacity: 0 ;' + 'transition: visibility 1s, opacity 0.8s linear;'
 
-        profile.style.visibility = 'visible'
-        profile.style.opacity = 1
-        profile.style.transition = 'visibility 0s, opacity 0.8s linear'
-        
     }
 }
 
